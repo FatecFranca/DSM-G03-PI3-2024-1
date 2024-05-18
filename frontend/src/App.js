6e09443efc8ae05fb99cee9cf918824abf2e8775
Pedro <Pedrogonmoreira@gmail.com>
2024-05-18 11:07:21 -0300

    atualização rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Services from "./pages/Services";
-import About from "./pages/About";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Services from './pages/Services';
+import About from './pages/About';
 
+const App = () => {
+  return (
+    <div>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+    </div>
+  );
+};
+
+export default App;
