071741465707d34de2c203ea94582bd534112f41
João Pedro Andrade Cintra <joao.cintra7@hotmail.com>
2024-06-06 14:58:13 -0300

    Adição do campo cadstro.
    Foi efetuado na pasta teste uma tela de cadastro de usuario, o mesmo já esta funcionando junto com o back-end, salvando os dados informados no MongoDB

--- teste/login-app/src/App.js
+++ teste/login-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import Signup from './Signup'; // Importar o componente de cadastro
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} /> {/* Adicionar rota para a página de cadastro */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
